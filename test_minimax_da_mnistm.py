471fe087909d845b7e0b12e2489a23124232d182
jihunhamm <jihun.hamm@gmail.com>
2018-05-27 01:43:51 -0400

    Add files via upload

--- test_minimax_da_mnistm.py
+++ test_minimax_da_mnistm.py
@@ -7,6 +7,9 @@ import tensorflow as tf
 import numpy as np
 import cPickle as pkl
 import matplotlib.pyplot as plt
+import keras
+#from keras.datasets import cifar10, cifar100
+from keras.datasets import mnist
 from utils import *
 
 
@@ -71,22 +74,19 @@ def minimax_kbeam(sess,feed_dict={}):
         sess.run(optim_max,feed_dict)
 
 
-def evaluate():
+def evaluate(X1test,X2test,y1test,y2test):
     batchsize = 1280
-    n = mnist_test.shape[0]
+    n = X1test.shape[0]
     nbatch = np.int(np.ceil(np.float(n)/np.float(batchsize)))
     err_test_src = 0.
     err_test_tar = 0.
     for i in range(nbatch):
         ind = range(batchsize*i,min(batchsize*(1+i),n))
-        err_test_src += sess.run(sumu,feed_dict={x:mnist_test[ind],yutil:mnist.test.labels[ind], batch_size:batchsize}) 
-        err_test_tar += sess.run(sumu,feed_dict={x:mnistm_test[ind],yutil:mnist.test.labels[ind],batch_size:batchsize})
+        err_test_src += sess.run(sumu,feed_dict={x:X1test[ind],yutil:y1test[ind], batch_size:batchsize}) 
+        err_test_tar += sess.run(sumu,feed_dict={x:X2test[ind],yutil:y2test[ind], batch_size:batchsize})
     err_test_src /= np.float(n)
     err_test_tar /= np.float(n)
-    '''        
-    for        
-        test_accp += sess.run(accp[id_max],feed_dict={x:mnist_test[:num_test],z:mnistm_test[:num_test],batch_size:num_test})
-    '''
+
     return [err_test_src,err_test_tar]
 
 
@@ -99,7 +99,7 @@ K = 2
 max_step = 1
 min_step = 1
 
-ntrial = 30
+ntrial = 10
 max_iter = 10001
 nskip = 100
 
@@ -112,33 +112,18 @@ batchsize = 128
 lr = 1E-2
 
 ## Load data
+(X1train, y1train), (X1test, y1test) = mnist.load_data()
+X1train = np.tile(X1train.reshape((-1,28,28,1)),(1,1,1,3))
+X1test = np.tile(X1test.reshape((-1,28,28,1)),(1,1,1,3))
+y1train = keras.utils.to_categorical(y1train,10)
+y1test = keras.utils.to_categorical(y1test,10)
 
-from tensorflow.examples.tutorials.mnist import input_data
-#import input_data
-mnist = input_data.read_data_sets('MNIST_data', one_hot=True)
-
-## Process MNIST
-mnist_train = (mnist.train.images > 0).reshape(55000, 28, 28, 1).astype(np.uint8) * 255
-mnist_train = np.concatenate([mnist_train, mnist_train, mnist_train], 3)
-mnist_test = (mnist.test.images >0).reshape(10000, 28, 28, 1).astype(np.uint8) * 255
-mnist_test = np.concatenate([mnist_test, mnist_test, mnist_test], 3)
 
 ## Load MNIST-M
 mnistm = pkl.load(open('mnistm_data.pkl'))
-mnistm_train = mnistm['train']
-mnistm_test = mnistm['test']
-mnistm_valid = mnistm['valid']
-
-'''
-# Compute pixel mean for normalizing data
-pixel_mean = np.vstack([mnist_train, mnistm_train]).mean((0, 1, 2))
-
-# Create a mixed dataset for TSNE visualization
-num_test = 500
-combined_test_imgs = np.vstack([mnist_test[:num_test], mnistm_test[:num_test]])
-combined_test_labels = np.vstack([mnist.test.labels[:num_test], mnist.test.labels[:num_test]])
-combined_test_domain = np.vstack([np.tile([1., 0.], [num_test, 1]), np.tile([0., 1.], [num_test, 1])])
-'''
+X2train = mnistm['train']
+X2test = mnistm['test']
+X2valid = mnistm['valid']
 
 
 ## Create the model
@@ -146,8 +131,6 @@ x = tf.placeholder(tf.uint8, [None,28,28,3],'x') # Source domain
 z = tf.placeholder(tf.uint8, [None,28,28,3],'z') # Target domain
 yutil = tf.placeholder(tf.float32, [None, Ku],'yutil')
 batch_size = tf.placeholder(tf.int32,[],'batch_size')
-#istrain = tf.placeholder(tf.bool, [])
-#lrate = tf.placeholder(tf.float32, [])
 
 xfloat = tf.multiply(tf.cast(x, tf.float32),0.0039215686)
 zfloat = tf.multiply(tf.cast(z, tf.float32),0.0039215686)
@@ -186,8 +169,7 @@ for i in range(K):
     vars_priv[i] = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, 'priv'+str(i))
     optim_min[i] = tf.train.MomentumOptimizer(lr,0.9).minimize(loss[i],var_list=vars_filt+vars_util)
     optim_max[i] = tf.train.MomentumOptimizer(lr,0.9).minimize(-loss[i],var_list=vars_priv[i])
-    #optim_min[i] = tf.train.AdamOptimizer(lr_filt).minimize(loss[i],var_list=vars_filt+vars_util)
-    #optim_max[i] = tf.train.AdamOptimizer(lr_priv).minimize(-loss[i],var_list=vars_priv[i])
+
 
 ## test accuracy
 iswrongu = tf.not_equal(tf.argmax(futilx,1), tf.argmax(yutil, 1))
@@ -199,14 +181,14 @@ saver = tf.train.Saver()
 
 #####################################################################################################################
 
-errs_src = np.nan*np.ones((int(np.ceil(max_iter/np.float(nskip))),ntrial))
-errs_tar = np.nan*np.ones((int(np.ceil(max_iter/np.float(nskip))),ntrial))
-
 sess = tf.Session()
 sess.run(tf.global_variables_initializer())
 
-gen_source_batch = batch_generator([mnist_train, mnist.train.labels], batchsize)
-gen_target_batch = batch_generator([mnistm_train, mnist.train.labels], batchsize)
+errs_src = np.nan*np.ones((int(np.ceil(max_iter/np.float(nskip))),ntrial))
+errs_tar = np.nan*np.ones((int(np.ceil(max_iter/np.float(nskip))),ntrial))
+
+gen_source_batch = batch_generator([X1train, y1train], batchsize)
+gen_target_batch = batch_generator([X2train, y1train], batchsize)
 
 print '\nK=%d, J=%d'%(K,max_step)
 
@@ -221,14 +203,8 @@ for trial in range(ntrial):
         feed_dict = {x:X, z:Z, yutil:Y, batch_size:batchsize}
         minimax_kbeam(sess, feed_dict)
         
-        if False:
-            fs = sess.run(loss,feed_dict)
-            id_max = np.argmax(fs) 
-            print 'step %d: f=%g, id_max=%d'%(it,fs[id_max],id_max)        
-            #print 'step %d: loss_u=%g, loss_p=%g, loss_joint=%g'%(it,lu,lp,ljoint)
-        
         if it%nskip == 0:
-            terr_src,terr_tar = evaluate()
+            terr_src,terr_tar = evaluate(X1test,X2test,y1test,y1test)
             errs_src[cnt,trial] = terr_src
             errs_tar[cnt,trial] = terr_tar
             cnt += 1
